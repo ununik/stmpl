01c462a154f38a6f52fe1545c4c131da60ca151d
Martin Pribyl <martin.pribyl@lingoking.com>
2015-10-12 12:24:30 +0200

    Add css style

--- views/entry-html.php
+++ views/entry-html.php
@@ -5,6 +5,6 @@
  * Date: 31. 7. 2015
  * Time: 16:56
  */
-$cont = "<h1>$entry->nadpis</h1>
+$cont = "<h1 class='entry-h1'>$entry->nadpis</h1>
          <div class='entry'>$entry->text</div>";
 return $cont;
