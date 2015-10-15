557f4e7b803d95247ea3df5f24371e4dca52b04e
Â= <=>
2015-10-15 07:25:19 +0200

    Some partners logos

--- views/sponzori-home.php
+++ views/sponzori-home.php
@@ -6,8 +6,14 @@
  * Time: 12:14
  */
 return "<div id='sponzori'><h2>Sponzoři</h2>
-        <a href='http://www.staremestopl.cz/' target='_blank' class='sponzor' title='Staré Město pod Landštejnem'><img src='images/sponzori/stareMestoPL.gif'></a>
-        <a href='http://www.kraj-jihocesky.cz/' target='_blank' class='sponzor' title='Jihočeský kraj'><img src='images/sponzori/jihoceskyKraj.jpg'></a>
-        <a href='http://www.ave.cz/' target='_blank' class='sponzor' title='AVE'><img src='images/sponzori/ave.png'></a>
+        <a href='http://www.staremestopl.cz/' target='_blank' class='sponzor' title='Staré Město pod Landštejnem'><img  alt='Staré Město pod Landštejnem' src='images/sponzori/stareMestoPL.gif'></a>
+        <a href='http://www.kraj-jihocesky.cz/' target='_blank' class='sponzor' title='Jihočeský kraj'><img  alt='Jihočeský kraj' src='images/sponzori/jihoceskyKraj.jpg'></a>
+        <a href='http://www.fondrbbinvest.cz/' target='_blank' class='sponzor' title='Nadační fond RBB Invest'><img  alt='Nadační fond RBB Invest' src='images/sponzori/rbb_invest.jpg'></a>
+        <a href='http://www.centropen.cz/' target='_blank' class='sponzor' title='Centropen'><img  alt='Centropen' src='images/sponzori/centropen.png'></a>
+        <a href='http://www.jednotajh.cz/' target='_blank' class='sponzor' title='Jednota Jindřichův Hradec'><img alt='Jednota Jindřichův Hradec' src='images/sponzori/jednota.jpg'></a>
+        <a href='http://www.agro-merin.cz/' target='_blank' class='sponzor' title='AGRO Měřín'><img alt='AGRO Měřín' src='images/sponzori/agro_miretin.gif'></a>
+        <a href='http://www.ave.cz/' target='_blank' class='sponzor' title='AVE'><img alt='AVE' src='images/sponzori/ave.png'></a>
+        <a href='http://www.csas.cz/' target='_blank' class='sponzor' title='Česká spořitelna'><img  alt='Česká spořitelna' src='images/sponzori/ceska_sporitelna.gif'></a>
+        <a href='http://www.wspk.cz/' target='_blank' class='sponzor' title='SPARKASSE'><img  alt='SPARKASSE' src='images/sponzori/sparkasse.jpg'></a>
 
 </div>";
