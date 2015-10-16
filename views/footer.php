3a115db18a94dcd0236208d21b2809f196c019d9
Martin Pribyl <martin.pribyl@lingoking.com>
2015-10-16 12:05:48 +0200

    Made better URL (just page=??)

--- views/footer.php
+++ views/footer.php
@@ -5,8 +5,8 @@
  * Date: 24. 7. 2015
  * Time: 16:22
  */
-return "<a href='' target='_blank' title='Facebook'><img src='images/footer/fb.png'></a>
+return "<a href='https://www.facebook.com/Klub-biatlonu-Star%C3%A9-M%C4%9Bsto-pod-Land%C5%A1tejnem-Klub-ze-srdce-%C4%8Cesk%C3%A9-kanady-109285809153540/' target='_blank' title='Facebook'><img src='images/footer/fb.png'></a>
         <a href='mailto:biatlon.stmesto@centrum.cz' title='Mail'><img src='images/footer/mail.png'></a>
         <a href='http://ununik.wz.cz/' target='_blank' title='Autor'><img src='images/footer/author.png'></a>
-        <a  href='index.php?page=archiv' title='Archiv'><img src='images/footer/archiv.png'></a>
-        <a  href='index.php?page=download' title='Ke stažení'><img src='images/footer/download.png'></a>";
+        <a  href='archiv' title='Archiv'><img src='images/footer/archiv.png'></a>
+        <a  href='download' title='Ke stažení'><img src='images/footer/download.png'></a>";
