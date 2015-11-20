2b6540e5a76fa22dc42979e2b92c95ac763a704a
Â= <=>
2015-11-20 07:28:37 +0100

    add new image kinesiotaping

--- views/kinesiotaping-html.php
+++ views/kinesiotaping-html.php
@@ -2,6 +2,7 @@
 $container = "<h1>Kinesiotaping</h1>";
 $container .= '<img src="images/kinesiotaping/postava_zaklad.png" usemap="#bodyMap">';
 $container .= '<map name="bodyMap">
+				  <area shape="poly" coords="125,450,110,495,180,495,145,450" title="Noha">
 				  <area shape="poly" coords="110,450,100,395,190,395,170,450" title="Holeň">
 				  <area shape="poly" coords="105,385,105,340,175,340,175,385" title="Koleno">
 		          <area shape="poly" coords="95,250,105,330,175,330,180,250,130,280" title="Stehno">
